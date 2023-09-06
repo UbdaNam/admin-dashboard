44ce1aa3402d5cd59b1ee010c598fb83b7d6a4e7
Abdurahim .M <Abdugreat2@gmail.com>
2023-09-06 15:04:46 +0300

    Add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Users from "./pages/users/Users";
+import Products from "./pages/products/Products";
+import Layout from "./layout/Layout";
+
 function App() {
-  return <div>Admin Dashboard</div>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "users",
+          element: <Users />,
+        },
+        {
+          path: "products",
+          element: <Products />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
