342c053f8b3d93348d0c40d4ae65898ecd19b51c
Abdurahim .M <Abdugreat2@gmail.com>
2023-09-14 23:39:00 +0300

    Add menu item template

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,7 +1,35 @@
+import { Link } from "react-router-dom";
 import "./menu.scss";
+import { LiaHomeSolid } from "react-icons/lia";
+import { BiGroup } from "react-icons/bi";
 
 const Menu = () => {
-  return <div className="menu">Menu</div>;
+  return (
+    <div className="menu">
+      <div className="item">
+        <h3 className="title">Main</h3>
+        <Link to="/" className="list-item">
+          <LiaHomeSolid />
+          <span className="menu-link">Home</span>
+        </Link>
+        <Link to="/profile" className="list-item">
+          <BiGroup />
+          <span className="menu-link">Profile</span>
+        </Link>
+      </div>
+      <div className="item">
+        <h3 className="title">Main</h3>
+        <Link to="/" className="list-item">
+          <LiaHomeSolid />
+          <span className="menu-link">Home</span>
+        </Link>
+        <Link to="/profile" className="list-item">
+          <BiGroup />
+          <span className="menu-link">Profile</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
