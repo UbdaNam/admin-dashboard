ed0cff18c2cbf8b4b959c42a5939761d7bb2c3cc
Abdurahim .M <Abdugreat2@gmail.com>
2023-09-14 23:43:13 +0300

    Update menu template sections

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -2,12 +2,13 @@ import { Link } from "react-router-dom";
 import "./menu.scss";
 import { LiaHomeSolid } from "react-icons/lia";
 import { BiGroup } from "react-icons/bi";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 
 const Menu = () => {
   return (
     <div className="menu">
       <div className="item">
-        <h3 className="title">Main</h3>
+        <h3 className="title">MAIN</h3>
         <Link to="/" className="list-item">
           <LiaHomeSolid />
           <span className="menu-link">Home</span>
@@ -18,14 +19,14 @@ const Menu = () => {
         </Link>
       </div>
       <div className="item">
-        <h3 className="title">Main</h3>
-        <Link to="/" className="list-item">
-          <LiaHomeSolid />
-          <span className="menu-link">Home</span>
-        </Link>
+        <h3 className="title">LISTS</h3>
         <Link to="/profile" className="list-item">
           <BiGroup />
-          <span className="menu-link">Profile</span>
+          <span className="menu-link">Users</span>
+        </Link>
+        <Link to="/" className="list-item">
+          <AiOutlineShoppingCart />
+          <span className="menu-link">Products</span>
         </Link>
       </div>
     </div>
