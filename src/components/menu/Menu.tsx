87519cc090b2cd80621634caccdc7a6523210a18
Abdurahim .M <Abdugreat2@gmail.com>
2023-09-15 23:11:56 +0300

    Add menu dynamically

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,34 +1,21 @@
 import { Link } from "react-router-dom";
 import "./menu.scss";
-import { LiaHomeSolid } from "react-icons/lia";
-import { BiGroup } from "react-icons/bi";
-import { AiOutlineShoppingCart } from "react-icons/ai";
+import { menu } from "../../data/menu";
 
 const Menu = () => {
   return (
     <div className="menu">
-      <div className="item">
-        <h3 className="title">MAIN</h3>
-        <Link to="/" className="list-item">
-          <LiaHomeSolid />
-          <span className="menu-link">Home</span>
-        </Link>
-        <Link to="/profile" className="list-item">
-          <BiGroup />
-          <span className="menu-link">Profile</span>
-        </Link>
-      </div>
-      <div className="item">
-        <h3 className="title">LISTS</h3>
-        <Link to="/profile" className="list-item">
-          <BiGroup />
-          <span className="menu-link">Users</span>
-        </Link>
-        <Link to="/" className="list-item">
-          <AiOutlineShoppingCart />
-          <span className="menu-link">Products</span>
-        </Link>
-      </div>
+      {menu.map(({ id, title, listItems }) => (
+        <div className="item" key={id}>
+          <h3 className="title">{title}</h3>
+          {listItems.map((listItem) => (
+            <Link to={listItem.url} className="list-item" key={listItem.id}>
+              {listItem.icon && <listItem.icon />}
+              <span className="menu-link">{listItem.title}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
     </div>
   );
 };
