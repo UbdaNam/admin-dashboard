9bf5f6e82e25ed23746e1922765aa0cbcae4fc4a
UbdaNam <abdugreat2@gmail.com>
2023-09-27 20:00:32 +0300

    Add mock data for top deals

---
+++ src/mock/top-deals.ts
@@ -0,0 +1,19 @@
+import { faker } from "@faker-js/faker";
+
+interface User {
+  id: number;
+  name: string;
+  avatar: string;
+  email: string;
+  amount: string;
+}
+
+const topDealUsers: User[] = new Array(10).fill({}).map(() => ({
+  id: parseInt(faker.string.numeric(10)),
+  name: faker.person.fullName(),
+  email: faker.internet.email(),
+  amount: faker.commerce.price({ min: 100, dec: 0, symbol: "$" }),
+  avatar: faker.image.avatar(),
+}));
+
+export default topDealUsers;
