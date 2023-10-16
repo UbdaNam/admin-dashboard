328d97e743280078c791b026cc8b6a1fd90d04f6
UbdaNam <abdugreat2@gmail.com>
2023-10-16 19:05:16 +0300

    Add piechart tips

--- src/components/pieChart/PieChart.tsx
+++ src/components/pieChart/PieChart.tsx
@@ -6,24 +6,37 @@ const PieChartBox = () => {
   return (
     <div className="pie-chart-wrapper">
       <h2>Leads by Source</h2>
-      <ResponsiveContainer width="99%" height={300}>
-        <PieChart>
-          <Tooltip
-            contentStyle={{ background: "#fff", borderRadius: "10px" }}
-          />
-          <Pie
-            data={data}
-            innerRadius={"70%"}
-            outerRadius={"90%"}
-            paddingAngle={5}
-            dataKey="value"
-          >
-            {data.map((item) => (
-              <Cell key={item.name} fill={item.color} />
-            ))}
-          </Pie>
-        </PieChart>
-      </ResponsiveContainer>
+      <div className="chart">
+        <ResponsiveContainer width="99%" height={300}>
+          <PieChart>
+            <Tooltip
+              contentStyle={{ background: "#fff", borderRadius: "10px" }}
+            />
+            <Pie
+              data={data}
+              innerRadius={"70%"}
+              outerRadius={"90%"}
+              paddingAngle={5}
+              dataKey="value"
+            >
+              {data.map((item) => (
+                <Cell key={item.name} fill={item.color} />
+              ))}
+            </Pie>
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
+      <div className="options">
+        {data.map((item) => (
+          <div className="option" key={item.name}>
+            <div className="title">
+              <div className="dot" style={{ backgroundColor: item.color }} />
+              <span>{item.name}</span>
+            </div>
+            <span>{item.value}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
