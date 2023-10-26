8b099d2f608ba29256ac70d91c57801d2a238e77
UbdaNam <abdugreat2@gmail.com>
2023-10-26 22:25:53 +0300

    Add user props and mock data types

--- src/mock/user-data.ts
+++ src/mock/user-data.ts
@@ -1,9 +1,10 @@
 import { faker } from '@faker-js/faker';
 
-export const usersRows = new Array(9).fill({}).map((_, index) => ({
+export const usersRows: object[] = new Array(9).fill({}).map((_, index) => ({
   id: index + 1,
   lastName: faker.person.firstName(),
   firstName: faker.person.lastName(),
   avatar: faker.image.avatar(),
   age: faker.datatype.number({ min: 18, max: 100 }),
+  status: faker.datatype.boolean(),
 }));
