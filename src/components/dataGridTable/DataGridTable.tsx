c4c31a8b0107e78b5c9274e78287d9bf2fff478f
UbdaNam <abdugreat2@gmail.com>
2023-10-26 22:25:21 +0300

    Make data grid table component reusable

--- src/components/dataGridTable/DataGridTable.tsx
+++ src/components/dataGridTable/DataGridTable.tsx
@@ -1,61 +1,19 @@
 import { DataGrid } from '@mui/x-data-grid';
 import { GridToolbar } from '@mui/x-data-grid/components';
-import {
-  GridColDef,
-  GridValueGetterParams,
-  GridRenderCellParams,
-} from '@mui/x-data-grid/models';
+import { GridColDef } from '@mui/x-data-grid/models';
 import './data-grid-table.scss';
-import { usersRows } from '../../mock/user-data';
 
-const DataGridTable = () => {
-  const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 90 },
-    {
-      field: 'avatar',
-      headerName: 'Avatar',
-      width: 100,
-      renderCell: (params: GridRenderCellParams) => {
-        return (
-          <img src={params.row.avatar} alt={params.row.firstName + ' avatar'} />
-        );
-      },
-    },
-    {
-      field: 'firstName',
-      headerName: 'First name',
-      width: 150,
-      editable: true,
-    },
-    {
-      field: 'lastName',
-      headerName: 'Last name',
-      width: 150,
-      editable: true,
-    },
-    {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
-      width: 110,
-      editable: true,
-    },
-    {
-      field: 'fullName',
-      headerName: 'Full name',
-      description: 'This column has a value getter and is not sortable.',
-      sortable: false,
-      width: 160,
-      valueGetter: (params: GridValueGetterParams) =>
-        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-    },
-  ];
+interface DataGridPropType {
+  rows: object[];
+  columns: GridColDef[];
+}
 
+const DataGridTable = ({ rows, columns }: DataGridPropType) => {
   return (
     <div className='data-table-container'>
       <DataGrid
         className='data-table'
-        rows={usersRows}
+        rows={rows}
         columns={columns}
         initialState={{
           pagination: {
