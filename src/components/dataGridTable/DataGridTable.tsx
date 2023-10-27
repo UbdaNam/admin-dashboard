3a88d4c987a6b6004c538403e663adca72481483
UbdaNam <abdugreat2@gmail.com>
2023-10-27 17:53:25 +0300

    Add actions for grid table

--- src/components/dataGridTable/DataGridTable.tsx
+++ src/components/dataGridTable/DataGridTable.tsx
@@ -1,24 +1,46 @@
 import { DataGrid } from '@mui/x-data-grid';
 import { GridToolbar } from '@mui/x-data-grid/components';
-import { GridColDef } from '@mui/x-data-grid/models';
+import { GridColDef, GridRenderCellParams } from '@mui/x-data-grid/models';
 import './data-grid-table.scss';
+import { Link } from 'react-router-dom';
+import { AiOutlineFundView } from 'react-icons/ai';
+import { MdDelete } from 'react-icons/md';
 
 interface DataGridPropType {
   rows: object[];
   columns: GridColDef[];
+  slug: string;
 }
 
-const DataGridTable = ({ rows, columns }: DataGridPropType) => {
+const DataGridTable = ({ rows, columns, slug }: DataGridPropType) => {
+  const actionColumn: GridColDef = {
+    field: 'actions',
+    headerName: 'Actions',
+    width: 200,
+    renderCell: (params: GridRenderCellParams) => {
+      return (
+        <div className='action flex-center'>
+          <Link to={`/${slug}/${params.row.id}`} className='view flex-center'>
+            <AiOutlineFundView />
+          </Link>
+          <button className='delete flex-center'>
+            <MdDelete />
+          </button>
+        </div>
+      );
+    },
+  };
+
   return (
     <div className='data-table-container'>
       <DataGrid
         className='data-table'
         rows={rows}
-        columns={columns}
+        columns={[...columns, actionColumn]}
         initialState={{
           pagination: {
             paginationModel: {
-              pageSize: 5,
+              pageSize: 10,
             },
           },
         }}
