6555473fb8e1b87f656ac7b46204759a14e251b2
UbdaNam <abdugreat2@gmail.com>
2023-11-05 22:25:59 +0300

    use details component for user details page

--- src/pages/user/User.tsx
+++ src/pages/user/User.tsx
@@ -1,7 +1,13 @@
+import Details from '../../components/details/Details';
+import { userDetail } from '../../mock/user-details';
 import './user.scss';
 
 const User = () => {
-  return <div className='user'>User</div>;
+  return (
+    <div className='user'>
+      <Details {...userDetail} />
+    </div>
+  );
 };
 
 export default User;
