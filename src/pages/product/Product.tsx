4899f994952c14ad0069c1adc75ea60afe3580e9
UbdaNam <abdugreat2@gmail.com>
2023-11-06 21:59:07 +0300

    Use details component for single product details

--- src/pages/product/Product.tsx
+++ src/pages/product/Product.tsx
@@ -1,7 +1,13 @@
+import Details from '../../components/details/Details';
+import { productDetails } from '../../mock/product-details';
 import './product.scss';
 
 const Product = () => {
-  return <div className='product'>Product</div>;
+  return (
+    <div className='product'>
+      <Details {...productDetails} />
+    </div>
+  );
 };
 
 export default Product;
